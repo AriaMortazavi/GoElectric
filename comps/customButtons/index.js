8987d07b556df89a9dc18ca5f3e9a4a6db0268f3
AriaMortazavi <ariamortazavi@ymail.com>
2020-04-13 11:18:02 -0700

    Added the comps folder and a button componenet

---
+++ comps/customButtons/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./cb.css";
+
+const CustomButton = ({text, color, fromclick}) => <div 
+    style={{backgroundColor:color, boxShadow:"1px 1px 5px" + color}}
+    className="custom_button_box">
+    <div className="custom_button_box_inner"
+    onClick={fromclick}>
+        {text}
+    </div>
+</div>
+
+function buttonClick(){
+}
+
+CustomButton.defaultProps = {
+    text: "Default Button",
+    color: "rgb(103, 215, 81)",
+    fromclick:buttonClick
+}
+export default CustomButton;
