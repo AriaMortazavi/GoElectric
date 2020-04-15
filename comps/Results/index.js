879cc6a7f60d1933927233887a790c9babfd4156
RogueWild <0.dreamer.009@gmail.com>
2020-04-14 18:14:30 -0700

    Results V1

--- comps/Results/index.js
+++ comps/Results/index.js
@@ -2,7 +2,32 @@ import React from 'react';
 import "./results.css";
 
 const Results = () => <div>
-    
+    <div id="results">
+        <div id="header">
+            By switching to electric:
+        </div>
+        <div id="saving">
+            <span>You are saving:</span><br />
+            $<span id="gas_save"></span> in gas per year
+        </div>
+        <div id="produce">
+            <span>You produce:</span><br />
+            <span id="carbon_produce"></span> less carbon per year
+        </div>
+        <div id="suggestion">
+            <span>We suggest these cars:</span>
+            <div id="cars_suggest">
+                <div id="cars_box">
+                    <div id="car_1">
+                        <div id="model_name">Model</div>
+                    </div>
+                    <div id="car_1">
+                        <div id="model_name">Model</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 export default Results;
---
+++ stories/2-Comps.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Results from '../comps/Results'
+
+export default {
+  title: 'Components',
+};
+
+export const ResultsComp = () => <Results />;
