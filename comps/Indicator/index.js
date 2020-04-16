73738367ca37c2b85b6e9894b60cdda5efb55832
RogueWild <0.dreamer.009@gmail.com>
2020-04-16 14:13:42 -0700

    Finished page indicator component

---
+++ comps/Indicator/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./indicator.css";
+
+const Indicator = () => <div>
+    <div id="indicator">
+        <div id="dot1"></div>
+        <div id="dot2"></div>
+        <div id="dot3"></div>
+
+        <div id="dot_active"></div>
+    </div>
+</div>
+
+export default Indicator;
--- stories/2-Comps.stories.js
+++ stories/2-Comps.stories.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import Results from '../comps/Results'
+import Indicator from '../comps/Indicator'
+
 
 export default {
   title: 'Components',
 };
 
 export const ResultsComp = () => <Results />;
+export const IndicatorComp = () => <Indicator />;
