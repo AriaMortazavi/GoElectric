5c03a1d6ba228a8780b97ba8bbf8c9c36380b5b5
RogueWild <0.dreamer.009@gmail.com>
2020-04-22 18:08:16 -0700

    Starting results page

--- pages/ResultsPage/index.js
+++ pages/ResultsPage/index.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import './resultspage.css';
+import Results from '../../comps/Results';
+
 
 const ResultsPage = () => {
-    
+
     return <div id="results_page">
-        page
+        <Results />
     </div>
 }
 
